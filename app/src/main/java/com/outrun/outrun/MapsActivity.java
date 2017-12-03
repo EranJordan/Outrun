068831208bb78ebe6e36c713673d9c59693c500d
EranJordan <eraneraneran11@gmail.com>
2017-12-03 19:43:47 +0200

    Test comment

--- app/src/main/java/com/outrun/outrun/MapsActivity.java
+++ app/src/main/java/com/outrun/outrun/MapsActivity.java
@@ -9,7 +9,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-
+//test comment
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
