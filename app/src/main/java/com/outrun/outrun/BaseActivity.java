2897f944ae8a1480002e51e84abd5bf60fc17251
Aviv Ples <aviv.ples1@gmail.com>
2017-12-04 17:53:19 +0200

    MainActivity with user redirection (according to login status) - redirect after sign in. Attempt to request permission. Logo and name update.

--- app/src/main/java/com/outrun/outrun/BaseActivity.java
+++ app/src/main/java/com/outrun/outrun/BaseActivity.java
@@ -1,8 +1,34 @@
 package com.outrun.outrun;
 
-/**
- * Created by AVIV on 04-Dec-17.
- */
+import android.app.ProgressDialog;
+import android.support.annotation.VisibleForTesting;
+import android.support.v7.app.AppCompatActivity;
 
-public class BaseActivity {
+public class BaseActivity extends AppCompatActivity {
+
+    @VisibleForTesting
+    public ProgressDialog mProgressDialog;
+
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        hideProgressDialog();
+    }
+
 }
---
+++ app/src/main/java/com/outrun/outrun/MainActivity.java
@@ -0,0 +1,145 @@
+package com.outrun.outrun;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.Manifest;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+
+public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback{
+
+    public final static int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private final static String TAG = "TestActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    protected void requestLocationPermission() {
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+            }
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Start the app
+
+                } else {
+                    System.exit(0); //should add message explaining
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    /* (non-Javadoc)
+   * @see android.app.Activity#onDestroy()
+   */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "On Destroy .....");
+    }
+    /* (non-Javadoc)
+    * @see android.app.Activity#onPause()
+    */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "On Pause .....");
+    }
+
+    /* (non-Javadoc)
+    * @see android.app.Activity#onRestart()
+    */
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "On Restart .....");
+    }
+
+    /* (non-Javadoc)
+    * @see android.app.Activity#onResume()
+    */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "On Resume .....");
+    }
+
+    /* (non-Javadoc)
+    * @see android.app.Activity#onStart()
+    */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        requestLocationPermission();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        redirectUI(account);
+        Log.i(TAG, "On Start .....");
+    }
+
+    protected void redirectUI(GoogleSignInAccount account) {
+        if(account == null) {
+            Intent signIn = new Intent(this, GoogleSignInActivity.class);
+            startActivity(signIn);
+        }
+        else{
+            Intent mainPage = new Intent(this, MapsActivity.class);
+            startActivity(mainPage);
+        }
+    }
+
+    /* (non-Javadoc)
+    * @see android.app.Activity#onStop()
+    */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "On Stop .....");
+    }
+}
